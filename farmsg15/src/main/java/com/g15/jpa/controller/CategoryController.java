6d047f185d98189f5e1b34d3b2eb87a503eea6f9
Edumolinamt <113559241+Edumolinamt@users.noreply.github.com>
2022-10-12 17:26:57 -0500

    Se actualizo crossOrigin

--- farmsg15/src/main/java/com/g15/jpa/controller/CategoryController.java
+++ farmsg15/src/main/java/com/g15/jpa/controller/CategoryController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/Category")
+@CrossOrigin(origins = "*")
 public class CategoryController {
     @Autowired
     private CategoryService categoryService;
